7972bdf75529b588552f52e7b1df1b39722462a4
adithyasreejithu <47926884+adithyasreejithu@users.noreply.github.com>
2025-04-22 01:46:12 -0400

    Fixed the Reading Issues and added a logger

--- Pdf_reader.py
+++ Pdf_reader.py
@@ -23,13 +23,49 @@ and find a better regrex statement to read it
 import os
 import re 
 import shutil
+import logging
 import db_func as func
 import numpy as np 
 from pypdf import PdfReader
 
+# Setting up Logging Operations 
+logger = logging.getLogger("pdf_Reader")
+logger.setLevel(logging.DEBUG)
+
+# File Handlers for Logging 
+file_handler = logging.FileHandler("file_processing.log")
+file_handler.setLevel(logging.DEBUG)
+
+# Creating Formatter 
+formatter = logging.Formatter(
+    "%(levelname)s - %(name)s: %(message)s"
+)
+
+# Adding formatter and handler to logger
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
 # Make this a dictionary later - use os lib to get file current location 
 folder_path = r"C:\Projects\Python\Stocks\Data\Wealthsimple_Files"  
 destination = r"C:\Projects\Python\Stocks\Data\Wealthsimple_Files\Scanned"
+
+
+def page_search(reader):
+    page_len = []
+    search_text = "Activity - Current period"
+
+    for i, page in enumerate(reader.pages):
+        lines = page.extract_text().split("\n") if page.extract_text() else []
+        if search_text in lines:
+            page_len.append(i)
+    return page_len
+
+def extract_pages(arr):
+    arr = np.array(arr)
+    max = arr.max()
+    min  = arr.min()
+    return (max,min)
 
 def move_file(file): 
     file_name = os.path.basename(file)
@@ -56,12 +92,50 @@ def unread_files():
                 files.append(file_location)
     return files
 
+def single_page(reader, min): 
+    logger.info("File only has data on a single page")
+    flag = False
+    page = reader.pages[int(min)]
+
+    read_values = np.array([])
+
+    for lines in page.extract_text().split("\n"):
+        if lines.__contains__("Activity - Current period"):
+            flag = True
+        
+        if flag == True:
+            read_values = np.append(read_values,lines)
+    return read_values
+
+def multiple_pages(reader, min, max):
+    logger.info("File only has data on multple pages")
+    flag = False 
+    read_values = np.array([])
+
+    for i in range(min, max+ 1):
+        page = reader.pages[i]
+        # print(f'this is {i}')
+        page_text = page.extract_text()
+
+        if not page_text:
+            continue  # skip if no extractable text
+
+        for line in page_text.split("\n"):
+            if "Activity - Current period" in line:
+                flag = True
+
+            if flag:
+                read_values = np.append(read_values, line)
+
+    # print(read_values)
+    return read_values
+
+    
 # Extracts and reads the pdf files
 def read_file(file_name):
-    
+
     i = 0
     index = 0
-    flag = False
     data = {
         "div": [],
         "buy": [],
@@ -69,24 +143,90 @@ def read_file(file_name):
         "lend": []
     }
 
-    # Retriving file location
-    file_location = os.path.abspath(file_name)
-    print(file_location)
-    
     # Reading file
-    reader = PdfReader(file_location)
-    page = reader.pages[1]
+    reader = PdfReader(file_name)
 
-    read_values = np.array([])
-    for lines in page.extract_text().split("\n"):
-        if lines.__contains__("Activity - Current period"):
-            flag = True
+    # Understanding page information
+    pagelen = page_search(reader)
+    max, min = extract_pages(pagelen)
+
+    logger.info(f'File data is being extracted from pages {pagelen}')
+
+    if min == max: 
+        read_values = single_page(reader, min)
+        # print(read_values)
+    
+    else:
+        read_values = multiple_pages(reader, min, max)
+        print("wellfuck")
+
+
+
+    # # page_search(reader)
+    # # page = reader.pages[1]
+
+    # read_pages = page_search(reader)
+    # max,min = extract_pages(read_pages)
+    # print(max)
+    # text = ""
+
+    # print(f"Number of pages: {len(reader.pages)}")
+    
+    # # Accessing the first page
+    # pagelen = page_search(reader)
+    # max, min = extract_pages(pagelen)
+    # print(f"\nthis is the min{min} and this is the max{max}")
+    # page = reader.pages[int(min)]
+    # print(page.extract_text())
+
+    
+    # for i in range(min, max):
+    #     page = reader.pages[i]
+    #     text = page.extract_text()
+    #     print(f"\n--- Page {i + 1} ---\n{text}")
+   
+    
+    # read_values = np.array([])
+    
+    # # Loop through the pages between min and max
+    # for page in reader.pages[min:max]:
+    #     text = page.extract_text() or ""
+    #     for line in text.split("\n"):
+    #         print(f"this is the {line}")
+    #         if line.__contains__("Activity - Current period"):
+    #             print("hello")
+    #             flag = True
+
+    #         if flag:
+    #             read_values = np.append(read_values, line)
+
+    # print(read_values)
+    # for lines in page.extract_text().split("\n"):
+    #     if lines.__contains__("Activity - Current period"):
+    #         flag = True
+        
+        
+    #     if flag == True:
+    #         read_values = np.append(read_values,lines)
+
+
+
+
+        # Extract text from the page and append it to 'text'
+        # page_text = page.extract_text() or ""  # In case extract_text returns None
+        # text += page_text + "\n"
+        # print(f"This is the text: {text}")
+
+    # read_values = np.array([])
+    # for lines in page.extract_text().split("\n"):
+    #     if lines.__contains__("Activity - Current period"):
+    #         flag = True
         
         # Will need an else statement to catch if it does not find the information
         # Most likely will be on the third page and will need to change line 37 "page = reader.pages[1]"
         
-        if flag == True:
-            read_values = np.append(read_values,lines)
+        # if flag == True:
+        #     read_values = np.append(read_values,lines)
 
     parsed = read_values[1:2]
     for value in parsed: 
@@ -153,6 +293,7 @@ def read_file(file_name):
             func.add_nine(stock_id,date, trans_type, recv_date, shares, fx_rate, amount1, amount2, amount3)
         else:
             print("No match found for:", item)  # Debugging
+            logger.debug(f" No Match found for: {item}")
 
     for item in data["buy"]:
 
@@ -170,6 +311,7 @@ def read_file(file_name):
             func.add_nine(stock_id,date, trans_type, recv_date, shares, fx_rate, amount1, amount2, amount3)
         else:
             print("No match found for:", item)  # Debugging
+            logger.debug(f" No Match found for: {item}")
 
     for item in data["cont"]:
 
@@ -187,7 +329,7 @@ def read_file(file_name):
             # func.add_four(date, amount1, amount2, amount3) 
         else:
             print("No match found for:", item)  # Debugging
-
+            logger.debug(f" No Match found for: {item}")
 
     for item in data["lend"]: 
 
@@ -204,6 +346,7 @@ def read_file(file_name):
             # func.add_four(date, amount1, amount2, amount3)            
         else: 
             print("No match found for:", item)  # Debugging
+            logger.debug(f" No Match found for: {item}")
         return data
 
 
@@ -213,9 +356,11 @@ check_folders(destination)
 
 # Checking what files do not exist 
 file_list = unread_files()
-# read_file(file_list[12])
+# read_file(file_list[6])
+
 
 for files in file_list:
+    logger.info(f"Started reading file: {os.path.basename(files)}")
     read_file(files)
     move_file(files)
     
